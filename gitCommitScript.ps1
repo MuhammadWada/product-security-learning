1c9fe261eb5165767ee7f001931dbb679dd4a040
Muhammad Wada <mwada@email.arizona.edu>
2026-02-16 18:51:28 -0700

    First commit of hello-world.py

---
+++ gitCommitScript.ps1
@@ -0,0 +1,23 @@
+#-----------------------------------------------------------------------------#
+# Script to pushing changes to the product-security-learning repo             #
+#-----------------------------------------------------------------------------#
+
+<#
+    I got this error when running the commit command for the first time:        
+        *** Please tell me who you are.
+        
+        Run
+        
+        git config --global user.email "you@example.com"
+        git config --global user.name "Your Name"
+        
+        to set your account's default identity.
+        Omit --global to set the identity only in this repository.
+        
+        fatal: unable to auto-detect email address (got 'mwada@Mobot.(none)')
+
+    
+#>
+
+git add .
+git 
